9f8eed35315e5f678183b98d5337b1218a91c1d3
km0930 <IsaacClark19900517@gmail.com>
2025-04-03 11:55:15 +0200

    Add sample test

---
+++ cypress/e2e/message_toggle.spec.js
@@ -0,0 +1,15 @@
+describe('Message Toggle Functionality', () => {
+  it('should display "Hello, World!" initially', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('Hello, World!').should('be.visible');
+  });
+
+  it('should toggle the message between "Hello, World!" and "Goodbye, World!" on button click', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('Toggle Message').click();
+    cy.contains('Goodbye, World!').should('be.visible');
+    cy.wait(500);
+    cy.contains('Toggle Message').click();
+    cy.contains('Hello, World!').should('be.visible');
+  });
+});
